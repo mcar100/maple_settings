d42af4374817972c200700885a60f4ba4098dc66
승섭kim <mcar100@naver.com>
2023-08-17 16:05:30 +0900

    Style: 제목 및 버전/날짜 위치 조정

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,6 +1,8 @@
-import react from 'react';
+import React, { useState } from 'react';
 
 function Header() {
+  const [version, setVersion] = useState('v.1.0');
+  const [lastUpdate, setLastUpdate] = useState('23.08.16');
   return (
     <div className="header-container">
       <div className="header-title">메이플 세팅 체크 리스트</div>
@@ -10,6 +12,8 @@ function Header() {
         <li>나만의 세팅</li>
       </ul> */}
       </div>
+      <div className="header-version">{version}</div>
+      <div className="header-lastupdate">{lastUpdate}</div>
     </div>
   );
 }
